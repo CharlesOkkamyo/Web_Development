920183d0a2f8ddb6479ab38a67eecb53ce3a4a6d
yotaro-redson <135019789+yotaro-redson@users.noreply.github.com>
2023-07-11 14:05:04 +0630

    Update Control.php

--- Control.php
+++ Control.php
@@ -1,1 +1,28 @@
+<?php 
+require_once 'Model.php';
 
+    class ShortenController {
+        private $urlModel;
+
+        public function __construct() {
+            $this->urlModel = new Model();
+        }
+
+        public function shortenUrl() {
+            if (isset($_POST['url'])) {
+                $originalUrl = $_POST['url'];
+
+                $shortCode = $this->urlModel->generateShortCode();
+                $shortenedUrl = "http://www.youtube.com/url_shortener/$shortCode";
+
+                $this->urlModel->saveShortURL($originalUrl, $shortenedUrl);
+
+                header("Location: index.php?shortened_url=$shortenedUrl");
+                exit;
+            }
+        }
+    }
+
+    $shortenController = new ShortenController();
+    $shortenController->shortenUrl();
+?>
